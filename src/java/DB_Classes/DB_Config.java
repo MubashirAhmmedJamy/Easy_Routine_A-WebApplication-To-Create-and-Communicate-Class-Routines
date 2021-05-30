ee2ea3ba03face9abefd72ee84da50120cf7bf02
Mubashir Ahmmed Jamy <54814463+MubashirAhmmedJamy@users.noreply.github.com>
2021-05-30 18:47:38 +0600

    Update DB_Config.java

--- src/java/DB_Classes/DB_Config.java
+++ src/java/DB_Classes/DB_Config.java
@@ -4,5 +4,5 @@ public class DB_Config {
     static String DRIVER_REGISTER = "com.mysql.jdbc.Driver";
     static String DB_LOCATION = "jdbc:mysql://localhost:3306/routinefinal";
     static String USERNAME = "root";
-    static String PASSWORD = "paradise46";
+    static String PASSWORD = "*******";
 }
